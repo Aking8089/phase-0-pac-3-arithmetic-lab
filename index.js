dade07807be6932080daf59bd3e6c71fd6fc161e
Ashley King <Princessak8918@hotmail.com>
2023-02-04 16:06:24 -0700

    pass test

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(){
+ 
+}
+function subtract(){
+
+}
+function multiply(){
+
+}
+function divide(a,b){
+return (a/b)
+}
+function add(a,b){
+return a + b;
+}
+function subtract (a,b){
+return a-b;
+}
+function multiply (a,b){
+return (a*b) 
+}
+function increment (n) {
+return (++n);
+}
+function decrement (n){
+return (--n)
+}
+function makeInt(n){
+return parseInt(n,10);
+}
+function preserveDecimal(n){
+ makeInt (n)
+}
+function preserveDecimal(n){
+makeInt (n)
+return parseFloat(n)
+}
